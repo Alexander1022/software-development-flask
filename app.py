3ac140561e4fb1817eea7a010bce80bdf834dabc
Alexander <36764735+Alexander1022@users.noreply.github.com>
2021-01-15 18:08:12 +0200

    Качвам basic неща по сайта
    Качвам basic неща по сайта :
     * Main menu
     * Posts Page
     * Users page с цветове, променящии се спрямо пола на човека
     * Register page

--- app.py
+++ app.py
@@ -1,10 +1,29 @@
 from flask import Flask, render_template
-
+from static_info import Posts, Users
 app = Flask(__name__)
+
+user_posts = Posts()
+p_users = Users()
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('mainpage.html')
+
+@app.route('/posts')
+def posts():
+    return render_template('posts.html', user_posts = user_posts)
+
+@app.route('/users')
+def users():
+    return render_template('users.html', users = p_users)
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ static_info.py
@@ -0,0 +1,45 @@
+def Posts():
+    posts = [
+        {
+            'id': 1,
+            'title':'Минута до полонощ',
+            'body':'Текст на книга',
+            'author':'Саманта',
+            'create_date':'01-15-2020'
+        },
+        {
+            'id': 2,
+            'title':'Тъмна стая',
+            'body':'Текст на книга',
+            'author':'Николай',
+            'create_date':'04-20-2016'
+        },
+        {
+            'id': 3,
+            'title':'Едно пътешествие',
+            'body':'Текст на книга',
+            'author': 'Мария',
+            'create_date':'04-01-2010'
+        }
+    ]
+    return posts
+
+def Users():
+    users = [
+        {
+            'name' : 'Саманта',
+            'created_profile' : '01-15-2020',
+            'sex' : 'f'
+        },
+        {
+            'name' : 'Николай',
+            'created_profile' : '04-20-2016',
+            'sex' : 'm'
+        },
+        {
+            'name' : 'Мария',
+            'created_profile' : '04-01-2010',
+            'sex' : 'f'
+        }
+    ]
+    return users
