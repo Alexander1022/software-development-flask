f004828b80cc902710d24231ec72897c523f7101
Vicktoria853 <48590084+Vicktoria853@users.noreply.github.com>
2021-01-27 20:15:19 +0200

    Add files via upload
    Добавяне  на клас за постове. Обвързване на потребителите с базата данни. Проблеми: няма съобщения за грешки при въвеждането на вече използващо се име при регистриране и някаква странна липса на активност при логването.

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask
-from flask import render_template, request, redirect, jsonify
+from flask import render_template, request, redirect, jsonify, flash, get_flashed_messages
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
 
 import hashlib
 import json
@@ -9,12 +11,28 @@ import os.path
 from itsdangerous import (TimedJSONWebSignatureSerializer as Serializer, BadSignature, SignatureExpired)
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.secret_key = "asdfghjkjytrewsdcv"
+
 db = SQLAlchemy(app)
 
 def hash_password(password):
     return hashlib.sha256(password.encode('utf-8')).hexdigest()
+
+def verify_token(token):
+    s = Serializer(app.secret_key)
+    try:
+        s.loads(token)
+    except SignatureExpired:
+        return False
+    except BadSignature:
+        return False
+    return True
+
+
+
+
 
 class User(db.Model):
     _id = db.Column(db.Integer, primary_key=True)
@@ -36,10 +54,38 @@ class User(db.Model):
         s = Serializer(app.secret_key, expires_in=600)
         return s.dumps({'username': self.username})
 
+    @staticmethod
+    def find_by_token(token):
+        if not token:
+            return None
+
+        try:
+            s = Serializer(app.secret_key)
+            payload = s.loads(token)
+            return User.query.filter_by(username=payload.get('username')).first()
+        except SignatureExpired:
+            return None
+
+
+
+class Post(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	author = db.Column(db.String(100), nullable=False)
+	content = db.Column(db.String(500), nullable=False)
+	#author = db.relationship('User', foreign_keys='User.id')
+	timestamp = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+
+
+
+
 db.create_all()
+
+
 
 @app.route('/')
 def index():
+    flash("Hello")
     return render_template('mainpage.html')
 
 @app.route('/posts')
@@ -50,28 +96,10 @@ def posts():
 def users():
     return render_template('users.html')
 
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-    if request.method == 'GET':
-        return render_template('register.html')
-
-    else:
-        username = request.form['username']
-        password = request.form['password']
-
-        try:
-            db.session.add(User(username=username, password=password))
-            db.session.commit()
-            return redirect('/')
-            flash('Регистрира се! Вече можеш да влезеш!', 'success')
-
-        except Exception as error: 
-            print(error)
-            return redirect(request.url)
-
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'GET':
+        #res = Post.query.all()
         return render_template('login.html')
     else:
         data = json.loads(request.data.decode('ascii'))
@@ -79,23 +107,42 @@ def login():
         password = data['password']
 
         user = User.query.filter_by(username=username).first()
-
-        if not user or not user.verify_password(password):
+        if not user or not user.verify_password(password):#hash_password(password)?
             return jsonify({'token': None})
-
         token = user.generate_token()
         return jsonify({'token': token.decode('ascii')})
 
-def is_logged_in(f):
-    @wraps(f)
-    def wrap(*args, **kwargs):
-        if 'logged_in' in session:
-            return f(*args, **kwargs)
-        else:
-            flash('Моля влезте!', 'danger')
-            return redirect(url_for('login'))
-    return wrap
 
-    
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    flash('Hello!')
+    if request.method == 'GET':
+        return render_template('register.html')
+
+    else:
+        username = request.form['username']
+        password = request.form['password']
+
+        if User.query.filter_by(username=username).first():
+			flash('Already taken username')
+        try:
+            user = User(username=username, password=password)
+            db.session.add(user)
+            db.session.commit()
+            return render_template('register.html')            
+            #return redirect('/')
+
+
+        except Exception as error: 
+            flash('Error : {}'.format(error))
+            return redirect(request.url)
+
+
+
+
+
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+
